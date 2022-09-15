0cbcc74e4187d6c38fe3ef8a8fa5297852c616b1
OlekSandr-offensive <oleksandr.tarasyuk92@gmail.com>
2022-09-15 16:10:51 +0200

    modified app

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-// const multer = require("multer");
-// const path = require("path");
-// const fs = require("fs/promises");
-// const crypto = require("crypto");
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -23,52 +19,6 @@ app.use(express.static("public"));
 
 app.use("/api/contacts", contactsRouter);
 app.use("/api/auth", authRouter);
-
-// const tempDir = path.join(__dirname, "temp");
-
-// const multerConfig = multer.diskStorage({
-//   destination: tempDir,
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   },
-// });
-
-// const upload = multer({
-//   storage: multerConfig,
-// });
-
-// const contacts = [];
-
-// const avatarDir = path.join(__dirname, "public", "avatars");
-
-// app.post(
-//   "/api/avatar",
-//   upload.single("Stay_with_Ukraine"),
-//   async (req, res) => {
-//     try {
-//       const { path: tempDir, filename } = req.file;
-//       const resultUpload = path.join(avatarDir, filename);
-//       await fs.rename(tempDir, resultUpload);
-//       const { name, email, phone } = req.body;
-//       const picture = path.join("avatars", filename);
-//       const newContact = {
-//         id: crypto.randomUUID(),
-//         name,
-//         email,
-//         phone,
-//         picture,
-//       };
-//       contacts.push(newContact);
-//       res.status(201).json(newContact);
-//     } catch (error) {
-//       await fs.unlink(req.file.path);
-//     }
-//   },
-// );
-
-// app.get("/api/avatar", async (req, res) => {
-//   res.json(contacts);
-// });
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
